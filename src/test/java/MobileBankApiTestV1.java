5c82a7d3c3b2beba8dff5cf673c7a96a35993c58
Вячеслав Поморцев <xaoasakura1991@gmail.com>
2023-09-07 19:56:21 +0300

    change json name

--- src/test/java/MobileBankApiTestV1.java
+++ src/test/java/MobileBankApiTestV1.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
